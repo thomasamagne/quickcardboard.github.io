2706b4488bbe3a8d4556b2308a45081e53781528
thomasamagne <31028368+thomasamagne@users.noreply.github.com>
2019-11-02 18:15:16 -0700

    first update

---
+++ references/ref.js
@@ -0,0 +1,9 @@
+function darken(x) {
+  
+  x.style.boxShadow="0px 0px 200px black inset"
+  x.style.transition = "all 0.25s"
+}
+function normal(x) {
+  x.style.boxShadow="none";
+  x.style.transition = "all 0.25s"
+}

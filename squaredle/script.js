1cec581228924ca0d7698abeb32e16c346204295
thomasamagne <31028368+thomasamagne@users.noreply.github.com>
2022-03-01 23:06:18 -0800

    Add files via upload

---
+++ squaredle/script.js
@@ -0,0 +1,1336 @@
+
+let dict = [
+'abet',
+'abut',
+'ache',
+'alit',
+'ante',
+'arch',
+'aver',
+'avow',
+'bach',
+'back',
+'bade',
+'bail',
+'bait',
+'bake',
+'bale',
+'balk',
+'ball',
+'band',
+'bang',
+'bank',
+'barb',
+'bard',
+'bare',
+'barf',
+'bark',
+'base',
+'bash',
+'bask',
+'bate',
+'bawl',
+'bead',
+'beam',
+'bean',
+'bear',
+'beat',
+'beef',
+'been',
+'beep',
+'bell',
+'belt',
+'bend',
+'bent',
+'bias',
+'bide',
+'biff',
+'bike',
+'bilk',
+'bill',
+'bind',
+'bird',
+'birl',
+'birr',
+'bite',
+'bitt',
+'blab',
+'blat',
+'bled',
+'blew',
+'blob',
+'blot',
+'blow',
+'blub',
+'blue',
+'blur',
+'boat',
+'bode',
+'body',
+'boil',
+'bolt',
+'bomb',
+'bond',
+'bone',
+'bong',
+'bonk',
+'book',
+'boom',
+'boot',
+'bore',
+'born',
+'boss',
+'bowl',
+'brag',
+'bray',
+'bred',
+'brew',
+'brim',
+'buck',
+'buff',
+'bulk',
+'bull',
+'bump',
+'bung',
+'bunk',
+'bunt',
+'buoy',
+'burl',
+'burn',
+'burp',
+'burr',
+'bury',
+'bush',
+'busk',
+'buss',
+'bust',
+'busy',
+'butt',
+'buzz',
+'caca',
+'cage',
+'cake',
+'calk',
+'call',
+'calm',
+'came',
+'camp',
+'cane',
+'cant',
+'card',
+'care',
+'cart',
+'case',
+'cash',
+'cast',
+'cave',
+'cede',
+'cere',
+'chap',
+'char',
+'chat',
+'chaw',
+'chid',
+'chin',
+'chip',
+'chop',
+'chug',
+'chum',
+'cite',
+'clad',
+'clam',
+'clap',
+'claw',
+'clew',
+'clip',
+'clog',
+'clop',
+'clot',
+'cloy',
+'club',
+'clue',
+'coal',
+'coat',
+'coax',
+'cock',
+'code',
+'coif',
+'coil',
+'coin',
+'coke',
+'comb',
+'come',
+'comp',
+'cone',
+'conk',
+'cook',
+'cool',
+'coop',
+'cope',
+'copy',
+'cord',
+'core',
+'cork',
+'corn',
+'cosh',
+'cost',
+'cowl',
+'crab',
+'cram',
+'crap',
+'crew',
+'crib',
+'crop',
+'crow',
+'cube',
+'cuff',
+'cull',
+'curb',
+'cure',
+'curl',
+'cuss',
+'dado',
+'damn',
+'damp',
+'dang',
+'dare',
+'darn',
+'dart',
+'dash',
+'date',
+'daub',
+'dawn',
+'daze',
+'deal',
+'deck',
+'deed',
+'deem',
+'defy',
+'deke',
+'demo',
+'dent',
+'deny',
+'dial',
+'dice',
+'died',
+'diet',
+'dike',
+'dine',
+'ding',
+'disc',
+'dish',
+'disk',
+'diss',
+'dive',
+'dock',
+'doff',
+'dole',
+'done',
+'doom',
+'dope',
+'doss',
+'dote',
+'dove',
+'down',
+'doze',
+'drag',
+'draw',
+'drew',
+'drip',
+'drop',
+'drub',
+'drug',
+'drum',
+'duck',
+'duel',
+'dull',
+'dump',
+'dung',
+'dunk',
+'dupe',
+'dusk',
+'dust',
+'earn',
+'ease',
+'echo',
+'edge',
+'edit',
+'emit',
+'envy',
+'espy',
+'etch',
+'even',
+'exit',
+'face',
+'fade',
+'fail',
+'fake',
+'fall',
+'fare',
+'farm',
+'fart',
+'fast',
+'fate',
+'fawn',
+'faze',
+'fear',
+'feed',
+'feel',
+'fell',
+'felt',
+'fend',
+'fess',
+'fete',
+'feud',
+'file',
+'fill',
+'film',
+'find',
+'fine',
+'fink',
+'fire',
+'firm',
+'fish',
+'fist',
+'fizz',
+'flag',
+'flap',
+'flat',
+'flaw',
+'flay',
+'fled',
+'flee',
+'flew',
+'flex',
+'flip',
+'flit',
+'flog',
+'flop',
+'flow',
+'flub',
+'flux',
+'foal',
+'foam',
+'foil',
+'fold',
+'fond',
+'fool',
+'foot',
+'ford',
+'fork',
+'form',
+'foul',
+'fowl',
+'frag',
+'frap',
+'fray',
+'free',
+'fret',
+'frit',
+'fuel',
+'full',
+'fume',
+'fund',
+'funk',
+'furl',
+'fuse',
+'fuss',
+'futz',
+'gain',
+'gait',
+'gall',
+'game',
+'gang',
+'gaol',
+'gape',
+'garb',
+'gash',
+'gasp',
+'gate',
+'gave',
+'gawk',
+'gawp',
+'gaze',
+'gear',
+'geld',
+'gibe',
+'gift',
+'gild',
+'gimp',
+'gird',
+'give',
+'glom',
+'glow',
+'glue',
+'glug',
+'glut',
+'gnaw',
+'goad',
+'golf',
+'gone',
+'gong',
+'goof',
+'gore',
+'gown',
+'grab',
+'gray',
+'grew',
+'grey',
+'grid',
+'grin',
+'grip',
+'grit',
+'grow',
+'grub',
+'gull',
+'gulp',
+'gush',
+'gust',
+'gybe',
+'hack',
+'hail',
+'hale',
+'halt',
+'hand',
+'hang',
+'hare',
+'hark',
+'harm',
+'harp',
+'hash',
+'hasp',
+'hast',
+'hate',
+'hath',
+'haul',
+'have',
+'hawk',
+'haze',
+'head',
+'heal',
+'heap',
+'hear',
+'heat',
+'heed',
+'heel',
+'heft',
+'held',
+'helm',
+'help',
+'herd',
+'hewn',
+'hide',
+'hike',
+'hill',
+'hint',
+'hire',
+'hiss',
+'hive',
+'hoax',
+'hock',
+'hold',
+'hole',
+'home',
+'hone',
+'honk',
+'hood',
+'hoof',
+'hook',
+'hoop',
+'hoot',
+'hope',
+'horn',
+'hose',
+'host',
+'hove',
+'howl',
+'huff',
+'hulk',
+'hull',
+'hump',
+'hung',
+'hunt',
+'hurl',
+'hurt',
+'hush',
+'husk',
+'hymn',
+'hype',
+'idle',
+'inch',
+'iron',
+'itch',
+'jack',
+'jade',
+'jail',
+'jape',
+'jazz',
+'jeer',
+'jell',
+'jerk',
+'jest',
+'jibe',
+'jilt',
+'jinx',
+'jive',
+'join',
+'joke',
+'jolt',
+'josh',
+'juke',
+'jump',
+'junk',
+'kayo',
+'keel',
+'keen',
+'keep',
+'kept',
+'kern',
+'kick',
+'kill',
+'kink',
+'kiss',
+'kite',
+'knap',
+'knew',
+'knit',
+'knot',
+'know',
+'lace',
+'lack',
+'laid',
+'lain',
+'lamb',
+'lame',
+'land',
+'lard',
+'lark',
+'lash',
+'last',
+'laud',
+'lave',
+'laze',
+'lead',
+'leaf',
+'leak',
+'lean',
+'leap',
+'leer',
+'left',
+'lend',
+'lent',
+'levy',
+'lick',
+'lift',
+'like',
+'lilt',
+'lime',
+'limp',
+'line',
+'link',
+'lisp',
+'list',
+'live',
+'load',
+'loaf',
+'loan',
+'lock',
+'loft',
+'loll',
+'long',
+'look',
+'loom',
+'loop',
+'loot',
+'lope',
+'lord',
+'lose',
+'lost',
+'lour',
+'love',
+'lube',
+'luck',
+'luff',
+'luge',
+'lull',
+'lump',
+'lure',
+'lurk',
+'lust',
+'made',
+'mail',
+'maim',
+'make',
+'mark',
+'mask',
+'mate',
+'maul',
+'meet',
+'meld',
+'melt',
+'mend',
+'meow',
+'mess',
+'miff',
+'milk',
+'mime',
+'mind',
+'mine',
+'miss',
+'moan',
+'mock',
+'moor',
+'moot',
+'mope',
+'move',
+'muck',
+'muff',
+'muse',
+'mush',
+'must',
+'mute',
+'nail',
+'name',
+'near',
+'neck',
+'need',
+'nest',
+'nick',
+'nock',
+'nose',
+'note',
+'nuke',
+'numb',
+'obey',
+'ogle',
+'okay',
+'omen',
+'omit',
+'ooze',
+'open',
+'oust',
+'pace',
+'pack',
+'page',
+'pain',
+'pair',
+'palm',
+'pant',
+'pare',
+'park',
+'part',
+'pass',
+'pave',
+'pawn',
+'peak',
+'peal',
+'peck',
+'peek',
+'peel',
+'peep',
+'peer',
+'perk',
+'pick',
+'pile',
+'pine',
+'plan',
+'plat',
+'play',
+'plod',
+'plop',
+'plot',
+'plow',
+'plug',
+'pock',
+'poke',
+'pool',
+'pore',
+'pose',
+'post',
+'pour',
+'pout',
+'pray',
+'prim',
+'prod',
+'prop',
+'puff',
+'pull',
+'pulp',
+'pump',
+'purl',
+'purr',
+'push',
+'putt',
+'quip',
+'quit',
+'quiz',
+'race',
+'rage',
+'raid',
+'rain',
+'rake',
+'rang',
+'rank',
+'rant',
+'rape',
+'rate',
+'raze',
+'read',
+'ream',
+'reap',
+'reef',
+'reek',
+'reel',
+'rein',
+'rely',
+'rend',
+'rent',
+'rest',
+'rice',
+'ride',
+'rile',
+'ring',
+'riot',
+'rise',
+'risk',
+'rive',
+'roam',
+'roar',
+'rock',
+'rode',
+'roil',
+'roll',
+'romp',
+'roof',
+'room',
+'root',
+'rope',
+'rout',
+'rove',
+'ruin',
+'rule',
+'rush',
+'rust',
+'sack',
+'said',
+'sail',
+'sale',
+'salt',
+'sass',
+'sate',
+'save',
+'sawn',
+'scab',
+'scam',
+'scan',
+'scar',
+'scat',
+'scud',
+'scum',
+'seal',
+'seam',
+'seat',
+'seed',
+'seek',
+'seem',
+'seen',
+'seep',
+'sell',
+'send',
+'sent',
+'sewn',
+'shag',
+'sham',
+'shed',
+'ship',
+'shit',
+'shoe',
+'shoo',
+'shop',
+'shot',
+'show',
+'shun',
+'shut',
+'side',
+'sift',
+'sigh',
+'sign',
+'sing',
+'sink',
+'sire',
+'site',
+'size',
+'skew',
+'skid',
+'skim',
+'skin',
+'skip',
+'slab',
+'slag',
+'slam',
+'slap',
+'slat',
+'slay',
+'sled',
+'slew',
+'slid',
+'slim',
+'slip',
+'slit',
+'slog',
+'slop',
+'slot',
+'slow',
+'slue',
+'slug',
+'slum',
+'slur',
+'smut',
+'snag',
+'snap',
+'snip',
+'snow',
+'snub',
+'soak',
+'soap',
+'soar',
+'sock',
+'soil',
+'sold',
+'sole',
+'solo',
+'sort',
+'sour',
+'sown',
+'spam',
+'span',
+'spar',
+'spat',
+'spay',
+'sped',
+'spew',
+'spin',
+'spit',
+'spot',
+'spud',
+'spur',
+'stab',
+'stag',
+'star',
+'stay',
+'stem',
+'step',
+'stet',
+'stew',
+'stir',
+'stop',
+'stow',
+'stub',
+'stud',
+'stun',
+'suck',
+'suit',
+'sulk',
+'sung',
+'sunk',
+'surf',
+'swab',
+'swam',
+'swap',
+'swat',
+'sway',
+'swig',
+'swim',
+'swob',
+'swot',
+'swum',
+'tack',
+'tail',
+'talc',
+'talk',
+'tame',
+'tamp',
+'tank',
+'tape',
+'task',
+'taxi',
+'team',
+'tear',
+'teem',
+'tell',
+'tend',
+'tent',
+'term',
+'test',
+'text',
+'thaw',
+'thin',
+'thud',
+'tick',
+'tide',
+'tile',
+'tilt',
+'time',
+'ting',
+'tint',
+'tire',
+'toil',
+'told',
+'toll',
+'tone',
+'took',
+'tool',
+'toot',
+'tope',
+'tore',
+'torn',
+'toss',
+'tote',
+'tour',
+'tout',
+'tram',
+'trap',
+'tree',
+'trek',
+'trim',
+'trip',
+'trod',
+'trot',
+'true',
+'tube',
+'tuck',
+'tune',
+'turf',
+'turn',
+'tusk',
+'twin',
+'twit',
+'type',
+'undo',
+'urge',
+'vamp',
+'vary',
+'veer',
+'veil',
+'vein',
+'vend',
+'vent',
+'vest',
+'veto',
+'vide',
+'view',
+'visa',
+'vise',
+'void',
+'vote',
+'wade',
+'waft',
+'wage',
+'wail',
+'wait',
+'wake',
+'walk',
+'wall',
+'wane',
+'want',
+'ward',
+'warm',
+'warn',
+'warp',
+'wash',
+'waul',
+'wave',
+'wawl',
+'wean',
+'wear',
+'weed',
+'weep',
+'weld',
+'well',
+'welt',
+'wend',
+'went',
+'wept',
+'were',
+'wert',
+'wham',
+'whap',
+'whet',
+'whip',
+'whir',
+'whiz',
+'whop',
+'will',
+'wilt',
+'wine',
+'wink',
+'wipe',
+'wire',
+'wish',
+'wist',
+'wive',
+'woke',
+'wolf',
+'woof',
+'word',
+'wore',
+'work',
+'worm',
+'worn',
+'wove',
+'wrap',
+'writ',
+'xray',
+'yack',
+'yank',
+'yarn',
+'yaup',
+'yawn',
+'yawp',
+'yean',
+'yell',
+'yelp',
+'yoke',
+'yowl',
+'zero',
+'zest',
+'zing',
+'zone',
+'zonk',
+'zoom',
+]
+
+
+//initialize global variables
+let guessGrid = document.querySelector("[data-guessgrid]")
+let tileList = document.getElementsByClassName('tile')
+let allGuess = []
+let guessCount = 0
+let guessBtn = document.getElementsByClassName('guess-count')
+let clueContainer = document.getElementsByClassName('clue-container')
+let alertContainer = document.querySelector('.alert-container')
+let shareBtn = document.querySelector('#share')
+let partialGuess = [null,null, null, null, null, null, null, null, null,null, null, null, null, null, null, null ]
+
+//initialize answer based on date
+let offsetFromDate = new Date(2022, 0, 1)
+let msOffset = Date.now() - offsetFromDate
+let dayOffset = Math.floor(msOffset/1000/60/60/24)
+
+let myrng = new Math.seedrandom(dayOffset)
+let myrngStr = myrng().toString()
+
+const word1 = dict[parseInt(myrngStr[2]+myrngStr[6]+myrngStr[10])];
+const word2 = dict[parseInt(myrngStr[3]+myrngStr[7]+myrngStr[11])];
+const word3 = dict[parseInt(myrngStr[4]+myrngStr[8]+myrngStr[12])];
+const word4 = dict[parseInt(myrngStr[5]+myrngStr[9]+myrngStr[13])];
+
+let answer = word1+word2+word3+word4
+let answerParsed = parseWords(answer)
+
+
+function parseWords(words) {
+  let parsedWord = [[],[],[],[],[],[],[],[]]
+  parsedWord[0] = words[0]+words[4]+words[8]+words[12]
+  parsedWord[1] = words[1]+words[5]+words[9]+words[13]
+  parsedWord[2] = words[2]+words[6]+words[10]+words[14]
+  parsedWord[3] = words[3]+words[7]+words[11]+words[15]
+  parsedWord[4] = words.slice(0,4)
+  parsedWord[5] = words.slice(4,8)
+  parsedWord[6] = words.slice(8,12)
+  parsedWord[7] = words.slice(12,16)
+
+  return parsedWord
+}
+
+
+function startInteraction() {
+  //add event listener to all tiles to select
+  for (let i = 0; i < tileList.length; i++) {
+  tileList[i].addEventListener('click', makeActive)
+  tileList[i].dataset.index = i
+}
+  //handle keyboard and mouse entry
+  document.addEventListener("click", handleMouseClick)
+  document.addEventListener("keydown", handleKeyPress)
+  //add event listener to all guess-count-containers
+  for (let i = 0; i < guessBtn.length; i++) {
+    guessBtn[i].dataset.index = i
+    guessBtn[i].addEventListener('click', toggleGuessBtn)
+  }
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick)
+  document.removeEventListener("keydown", handleKeyPress)
+}
+
+startInteraction()
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")){
+    pressKey(e.target.dataset.key)
+    return
+  }
+  if (e.target.matches("[data-enter]")) {
+    submitGuess(guessCount)
+    return
+  }
+  if (e.target.matches("[data-delete]")) {
+    deleteKey()
+    return
+  }
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess(guessCount)
+    return
+  }
+  if (e.key ==="Backspace"||e.key === "Delete") {
+    deleteKey()
+    return
+  }
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key)
+    return
+  }
+}
+
+function pressKey(key) {
+  //find the active tile
+  let currentTile = document.querySelector('[data-state="active"]')
+  //enter the letter
+  currentTile.textContent = key.toLowerCase()
+  currentTile.dataset.state = 'hasLetter';
+  //move the active tile
+  if (currentTile.dataset.index < 15) {
+    currentTile.nextElementSibling.dataset.state = 'active'
+  } else {
+    currentTile.dataset.state = 'active'
+  } 
+}
+
+function deleteKey() {
+  let currentTile = document.querySelector('[data-state="active"]')
+  currentTile.textContent = ''
+  if (currentTile.dataset.index > 0) {
+    currentTile.dataset.state = ''
+    currentTile.previousElementSibling.dataset.state = 'active'
+  }
+  
+}
+
+function submitGuess(gc) {
+      guessCount ++
+  //check win
+  if (saveGuess() === answer) {
+    alert('You got it in '+ (guessCount + 1) + ' guesses!', true)
+    stopInteraction()
+    for (let i = 0; i < guessBtn.length; i++) {
+      guessBtn[i].removeEventListener('click', toggleGuessBtn)
+     }
+    //win animation
+    var interval = 100;
+
+    for (let i = 0; i < tileList.length; i++) {
+      setTimeout(function () {
+        tileList[i].classList.add('win')
+    }, i * interval)
+}
+
+    
+  }
+  //check valid
+  if (checkValid()) {
+    allGuess[gc] = saveGuess()
+    renderClues(gc)
+    makeBtnActive()
+
+    stopInteraction()
+    for (let i = 0; i< tileList.length; i++) {
+      tileList[i].addEventListener('click', highlight)
+    }
+  } else {
+    alert('Invalid Guess')
+  }
+  partialGuess = [null,null, null, null, null, null, null, null, null,null, null, null, null, null, null, null ]
+  //check loss
+  if (guessCount > 6) {
+    alert('Nice try, but the answer was ' + word1 + ", "+ word2 + ", "+ word3 + ", and "+ word4 , true)
+  }
+}
+
+
+
+//Click tile to make active
+function makeActive(e) {
+  //remove all other active states
+  for (let i = 0; i < tileList.length; i++) {
+    tileList[i].dataset.state = ''
+  }
+  e.currentTarget.dataset.state = 'active'
+}
+
+//toggle guess buttons
+function toggleGuessBtn(num) {
+  if (this.dataset.state != 'inactive') {
+    for (let i = 0; i < guessBtn.length; i++) {
+      guessBtn[i].dataset.selection = 'false'
+    }
+    //reset grid selection to first tile
+    for (let i = 0; i < tileList.length; i++) {
+      tileList[i].dataset.state = ''
+    }
+    tileList[0].dataset.state = 'active'
+    this.dataset.selection = 'true'
+  }
+  
+  if (this.dataset.state == 'read') {
+    savePartial()
+    stopInteraction()
+    clearGrid()
+    for (let i = 0; i< tileList.length; i++) {
+      tileList[i].textContent = allGuess[parseInt(this.dataset.index)][i]
+      tileList[i].addEventListener('click', highlight)
+    } 
+    renderClues(this.dataset.index)
+  } else if (this.dataset.state == 'current') {
+      startInteraction()
+    for (let i = 0; i< tileList.length; i++) {
+      tileList[i].removeEventListener('click', highlight)
+      tileList[i].classList.remove('highlight')
+    } 
+      clearGrid()
+      drawPartial()
+    }
+
+}
+
+//save guess
+function saveGuess() {
+  let currentGuess =''
+  for (let i = 0; i < 16; i++) {
+    currentGuess = currentGuess + tileList[i].textContent
+  }
+  return currentGuess
+  
+}
+
+//render clues
+function renderClues(gc) {
+  
+  let guessParsed = parseWords(allGuess[gc])
+  //loop through all 8 clues
+  for (let i = 0; i< guessParsed.length; i++) {
+    let clueArray = []
+    let workingGuessParsed = guessParsed[i]
+    let workingAnswerParsed = answerParsed[i]
+    //check for exact matches, remove letters from exact matches
+    for (let j = 0; j< workingGuessParsed.length; j++) {
+      if(workingGuessParsed[j]===workingAnswerParsed[j]) {
+        clueArray.push(2)
+        workingGuessParsed = workingGuessParsed.substring(0, j) + workingGuessParsed.substring(j+1, workingGuessParsed.length)
+        workingAnswerParsed = workingAnswerParsed.substring(0, j) + workingAnswerParsed.substring(j+1, workingAnswerParsed.length)
+        j = -1; continue
+      } 
+    }
+    //check for right letter wrong position from the left overs
+    for (let j = 0; j< workingGuessParsed.length; j++) {
+      if (workingAnswerParsed.includes(workingGuessParsed[j])){
+        clueArray.push(1)
+      }
+    }
+    //draw out the clues based on clueArray
+    for (let j = 0; j < clueArray.length; j++) {
+      let clueCell = document.createElement('div')
+      clueCell.classList.add('clue')
+      clueContainer[i].append(clueCell)
+      if (clueArray[j] == 2) {
+        clueCell.dataset.state = 'correct'
+      } else if (clueArray[j] == 1) {
+        clueCell.dataset.state = 'wrong-position'
+      }
+    }
+  }
+}
+
+//make button active
+function makeBtnActive () {
+  let currentButton = document.querySelector("[data-state = 'current']")
+  let nextButton = currentButton.nextElementSibling
+  currentButton.dataset.state = 'read'
+  nextButton.dataset.state = 'current'
+}
+
+function clearGrid(){
+  for (let i = 0; i< tileList.length; i++) {
+    tileList[i].textContent = ''
+  }
+  //clear all child elements from clue container
+  for (let i = 0; i<clueContainer.length; i++) {
+    while (clueContainer[i].firstChild) {
+      clueContainer[i].removeChild(clueContainer[i].firstChild)
+    }
+  }
+}
+
+// Check if all tiles are filled out
+function checkValid() {
+  let currentGuess =''
+  for (let i = 0; i < tileList.length; i++) {
+    currentGuess = currentGuess + tileList[i].textContent
+  }
+  if (currentGuess.length <16) {
+    alert("Invalid Guess")
+  } else {
+    return true
+  }
+}
+
+function alert(text, win) {
+  
+  alertContainer.classList.add('show')
+  alertContainer.textContent = text
+  if (win != true) {
+    setTimeout(function() {
+    alertContainer.classList.remove('show')
+  }, 2000)
+}
+
+  }
+  
+function savePartial() {
+  let checkForNull = []
+  
+    for (let i = 0; i< tileList.length; i++) {
+      checkForNull.push(tileList[i].textContent)
+      } 
+    if (checkForNull.includes('')) {
+      for (let i = 0; i<tileList.length; i++) {
+        if (tileList[i].textContent != '') {
+          partialGuess[i]= tileList[i].textContent
+        }
+      }
+    }
+  }
+
+
+function drawPartial() {
+  for (let i = 0; i<tileList.length; i++) {
+    if (partialGuess[i]) {
+      tileList[i].textContent = partialGuess[i]
+    } 
+  }
+}
+
+function highlight() {
+  this.classList.toggle('highlight')
+  partialGuess = [null,null, null, null, null, null, null, null, null,null, null, null, null, null, null, null ]
+  
+  for (let i = 0; i< tileList.length; i++) {
+    if (tileList[i].classList.contains('highlight')) {
+      partialGuess[i] = tileList[i].textContent
+    } 
+  }
+}
